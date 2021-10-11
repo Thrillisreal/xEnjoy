8c92f03aa2f9cc096a5ea80a6d2e35365690e99e
Micah <micahstewart324@gmail.com>
2021-10-11 16:21:32 -0400

    imports

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div>
+
+  </div>
 </template>
 
 <script>
+import Anime from '../components/Anime'
+import Comment from '../components/Comment'
 export default {
+  name: 'Home',
+  components: {
+  Anime, 
+  Comment
+  }
+  ,
+  data: ()=>{
+
+  }
 
 }
 </script>
