5263a555eddba8d42b2099eeb18e8c3738a9b058
Micah <micahstewart324@gmail.com>
2021-10-13 10:29:19 -0400

    signIn setup

--- client/src/pages/SignIn.vue
+++ client/src/pages/SignIn.vue
@@ -1,9 +1,43 @@
 <template>
-  <div></div>
+  <div >
+    <div>
+      <h1>Sign In</h1>
+      <form @submit.prevent="onSubmit" >
+        <div>
+          <h2>Email:</h2>
+          <input type="email" :value="email" @input="handleChange"  />
+        </div>
+        <div>
+          <h2>Password:</h2>
+          <input type="password" :value="password" @input="handlePass" />
+        </div>
+        <div class="linkdiv">
+          <a href="/">New Account?</a>
+        </div>
+        <button type="submit" :disabled="username ==='' || password === ''">Sign In</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'SignIn',
+  components: {},
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+  methods: {
+    handleChange(event){
+      this.username = event.target.value
+    },
+    handlePass(event){
+      this.password = event.target.value
+    },
+    async onSubmit(){
+      
+    }
+  }
 }
 </script>
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -2,8 +2,23 @@ const router = require('express').Router()
 const controller = require('../controllers/CommentController')
 const middleware = require('../middleware')
 
-router.get('/allcomments', controller.GetAllComments)
-router.post('/newcomment', controller.CreateComment)
-router.get('/search/id/:comment_id', controller.FindCommentById)
+router.get(
+  '/allcomments',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetAllComments
+)
+router.post(
+  '/newcomment',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateComment
+)
+router.get(
+  '/search/id/:comment_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.FindCommentById
+)
 
 module.exports = router
