7081feeaa0c4802cf37175ad2bac30711f577898
Micah <micahstewart324@gmail.com>
2021-10-18 08:37:13 -0400

    delete commented out14

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const controller = require('../controllers/UserController')
-// const middleware = require('../middleware')
 
 router.post('/login', controller.LogUserIn)
 router.post('/register', controller.CreateNewUser)
