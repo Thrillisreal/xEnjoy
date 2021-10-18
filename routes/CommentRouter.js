1e1cbf4dd71b23d23181c12c8bfa52021e2eebae
Micah <micahstewart324@gmail.com>
2021-10-18 08:36:18 -0400

    delete commented out10

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const controller = require('../controllers/CommentController')
-// const middleware = require('../middleware')
 
 router.get(
   '/allcomments',
