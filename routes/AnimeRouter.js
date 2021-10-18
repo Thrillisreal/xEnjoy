4aa601edc7ef6ffcc61cf61c32867820524b3d38
Micah <micahstewart324@gmail.com>
2021-10-18 08:37:30 -0400

    delete commented out15

--- routes/AnimeRouter.js
+++ routes/AnimeRouter.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const controller = require('../controllers/AnimeController')
-// const middleware = require('../middleware')
 
 router.post('/watchlist', controller.postAnime)
 
